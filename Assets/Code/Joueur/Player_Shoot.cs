947ab29dcf0c2c8ee242d7bcca2983df6e26d2e6
Arthur_Perret <arthur.perret@univ-lyon2.fr>
2025-12-16 14:58:05 +0100

    Reload

--- Assets/Code/Joueur/Player_Shoot.cs
+++ Assets/Code/Joueur/Player_Shoot.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Player_Shoot : MonoBehaviour
 {
     public Camera cam;
+    public int bullets = 6;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Shoot();
+            if (bullets > 0)
+            {
+                Shoot();
+            }
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            Realod();
         }
     }
 
     void Shoot()
     {
+        bullets--;
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
@@ -26,5 +35,12 @@ public class Player_Shoot : MonoBehaviour
                 enemy.TakeDamage(1f);
             }
         }
+        Debug.Log(bullets);
+    }
+
+    void Realod() 
+    {
+        bullets = 6;
+        Debug.Log(bullets);
     }
 }
