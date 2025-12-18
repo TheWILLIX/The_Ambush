a5263361dbfb3b2095f51f8a47f943812231af0c
Arthur_Perret <arthur.perret@univ-lyon2.fr>
2025-12-18 12:06:42 +0100

    Respawn Ennemis sur des spawns différents

--- Assets/Code/Enemy/Wave.cs
+++ Assets/Code/Enemy/Wave.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,8 @@ using UnityEngine;
 public class Wave : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    public Transform[] spawnPoints;
+    public List<Transform> spawnPoints;
+    public List<Transform> spawnPointsUsed;
 
     public int waveNumber = 1;
     public float timeBetweenWaves = 3f;
@@ -23,21 +25,46 @@ public class Wave : MonoBehaviour
 
     IEnumerator StartNextWave()
     {
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
+            spawnPointsUsed.Add(null);
+        }
         waveInProgress = true;
 
         Debug.Log("Vague " + waveNumber + " commence dans " + timeBetweenWaves + " secondes.");
         yield return new WaitForSeconds(timeBetweenWaves);
 
-        int enemyCount = waveNumber * 3;
+        int enemyCount = waveNumber;
+
+        // temporaire à modifier
+        if (enemyCount > 10) { enemyCount = 10; }
+
 
         for (int i = 0; i < enemyCount; i++)
         {
-            Transform spawn = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            
+            int random_id = UnityEngine.Random.Range(0, spawnPoints.Count);
+            Transform spawn = spawnPoints[random_id];
+
+            spawnPoints.RemoveAt(random_id);
+            spawnPointsUsed[i] = spawn;
             Instantiate(enemyPrefab, spawn.position, spawn.rotation);
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.4f);
+        }
+
+
+        for (int i = spawnPointsUsed.Count - 1; i > -1; i--)
+        {
+            if (spawnPointsUsed[i] != null) 
+            { 
+                spawnPoints.Add(spawnPointsUsed[i]);
+            }
+            spawnPointsUsed.RemoveAt(i);
         }
 
         waveNumber++;
         waveInProgress = false;
+        Debug.Log(waveInProgress);
     }
+
 }
